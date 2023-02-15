1f8bd9ea2fffb143c1b593a00472eb5e644510f7
이건호 <hoyahoya0517@naver.com>
2023-02-15 15:57:00 +0900

    login fix

--- src/api/firebase.jsx
+++ src/api/firebase.jsx
@@ -21,9 +21,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
-  prompt: "select_account",
-});
+// provider.setCustomParameters({
+//   prompt: "select_account",
+// });
 const auth = getAuth(app);
 const db = getDatabase();
 
